c5c2f4291cae27d482ec66f7c9e1379e205b7048
manan36chauhan <cmanan36@gmail.com>
2023-10-22 16:27:54 -0700

    card

--- src/features/restaurants/components/restaurant-info-component.js
+++ src/features/restaurants/components/restaurant-info-component.js
@@ -1,3 +1,37 @@
 import react from "react";
-import { Text } from "react-native-paper";
-export const RestaurantInfo = () => <Text>Text--=-==-=-=-=</Text>;
+import { Text, Card } from "react-native-paper";
+import { StyleSheet } from "react-native";
+
+// import styled from "styled-component";
+// const Title = styled.Text`
+//   padding: 16px;
+//   color: red;
+// `;
+
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "some restaurants",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2023/05/pizza-in-a-paper-box.jpg",
+    ],
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <Card elevation={5}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      {/* <Title>{name}</Title> */}
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: { padding: 20, backgroundColor: "white" },
+  title: { padding: 16 },
+});
